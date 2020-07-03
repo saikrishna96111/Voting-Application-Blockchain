df079d6bb79b18290ab18e2b249c54121253d1b2
saikrishna96111 <saikrishna96111@gmail.com>
2020-07-03 08:46:27 +0530

    List candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,8 +2,28 @@ pragma solidity ^0.5.16;
 
 contract Election {
 
-	string public candidate;
+	//Model a Candidate
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
+
+	//store candidates
+	mapping(uint => Candidate) public candidates;
+
+	//store candidates count
+	uint public candidatesCount;
+
+	// string public candidate;
 	constructor () public {
-		candidate = "Candidate 1";
+		// candidate = "Candidate 1";
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+
+	function addCandidate (string memory _name) private {
+		candidatesCount ++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
 	}
 }
